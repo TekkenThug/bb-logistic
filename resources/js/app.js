5e3e8fb4c496e5a4570b5a9b9fe344251474eda4
Vadim I <ignatov0131@gmail.com>
2021-05-29 11:56:39 +0300

    Добавлен компонент модального окна

--- resources/js/app.js
+++ resources/js/app.js
@@ -22,6 +22,7 @@ window.Vue = require('vue').default;
 
 Vue.component('v-footer', require('./components/Footer.vue').default);
 Vue.component('preloader', require('./components/Preloader.vue').default);
+Vue.component('modal-window', require('./components/ModalWindow.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
