9c357dc12ffdc7f960e86ec5eb840a1753b98e24
Vadim Ignatov <ignatov0131@gmail.com>
2022-09-10 14:30:21 +0300

    chore: create alert component

---
+++ resources/js/components/ui/v-alert/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    :class="[
+      'alert',
+      type && `alert-${type}`
+    ]"
+    role="alert"
+  >
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+ name: 'VAlert',
+
+ props: {
+  type: {
+    type: String,
+    default: 'info'
+  }
+ }
+}
+</script>
+
+<style>
+
+</style>
