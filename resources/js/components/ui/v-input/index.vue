303051de43f9e801db0f89d91bb67ad260e0b0e0
Vadim Ignatov <ignatov0131@gmail.com>
2022-09-10 13:34:39 +0300

    chore: create v input

---
+++ resources/js/components/ui/v-input/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="v-input">
+    <h3 v-if="label">
+      {{ label }}
+    </h3>
+
+    <input
+      class="form-control"
+      :type="type"
+      :value="value"
+      :required="required"
+      :placeholder="placeholder"
+      @change="$emit(event.target.value)"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VInput',
+
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+
+    value: {
+      type: [String, Number],
+      default: ''
+    },
+
+    placeholder: {
+      type: [String, Number],
+      default: ''
+    },
+
+    type: {
+      type: String,
+      default: 'text'
+    },
+
+    required: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
