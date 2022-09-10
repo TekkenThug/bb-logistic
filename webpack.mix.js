d20769b4ed310bdc056e4c8d94735c7c47b5f107
Vadim Ignatov <ignatov0131@gmail.com>
2022-09-10 11:15:20 +0300

    refactor: add alias for mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+const path = require('path');
 
 /*
  |--------------------------------------------------------------------------
@@ -10,6 +11,10 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+
+mix.alias({
+    '@': path.join(__dirname, 'resources/js'),
+})
 
 mix.js('resources/js/app.js', 'public/js')
     .vue()
