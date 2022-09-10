7d1a1b9340215b4e3587b9ba5432c74f9ec6a4a7
Vadim Ignatov <ignatov0131@gmail.com>
2022-09-10 14:10:07 +0300

    chore: create v button component

---
+++ resources/js/components/ui/v-button/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <button
+    :class="[
+      'btn',
+      'btn-primary',
+      {
+        'w-100': fullSized
+      }
+    ]"
+    @click="$emit('click')"
+  >
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'VButton',
+
+  props: {
+    fullSized: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
