28608aea81520fc5084969340e3ade01beb5dc79
Vadim Ignatov <ignatov0131@gmail.com>
2024-02-25 10:35:37 +0300

    refactor: replace styles

--- webpack.mix.js
+++ webpack.mix.js
@@ -18,4 +18,4 @@ mix.alias({
 
 mix.js('resources/js/app.js', 'public/js')
     .vue()
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/frontend/assets/styles/global.scss', 'public/css');
