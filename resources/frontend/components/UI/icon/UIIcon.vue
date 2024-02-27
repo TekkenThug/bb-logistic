02df5b9c72c5d2216f42513f2d4ae680bcbe0b43
Vadim Ignatov <ignatov0131@gmail.com>
2024-02-27 16:34:09 +0300

    feat: create icon component

---
+++ resources/frontend/components/UI/icon/UIIcon.vue
@@ -0,0 +1,28 @@
+<template>
+    <i :class="classes"></i>
+</template>
+
+<script setup>
+import { computed } from "vue";
+
+import 'primeicons/primeicons.css';
+
+const props = defineProps({
+    name: {
+        type: String,
+        required: true,
+    },
+});
+
+const classes = computed(() => ['pi', `pi-${props.name}`]);
+</script>
+
+<style>
+    :root {
+        --ui-icon-size: 16px;
+    }
+
+    .pi {
+        font-size: var(--ui-icon-size);
+    }
+</style>
