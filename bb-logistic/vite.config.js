583e0ca2fb2d9a14b29e1f2d6fe3b47c94a53975
Vadim Ignatov <ignatov0131@gmail.com>
2024-02-28 19:30:53 +0300

    refactor: replace vite config

--- bb-logistic/vite.config.js
+++ bb-logistic/vite.config.js
@@ -1,11 +1,33 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/frontend/main.js"],
             refresh: true,
         }),
+        vue(),
     ],
-});
+
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: "@use './resources/frontend/assets/styles/exported' as *;",
+            },
+        },
+    },
+
+    resolve: {
+        alias: {
+            "@": "/resources/frontend",
+        },
+        extensions: ['.vue', '.js', '.ts', '.tsx', '.jsx'],
+    },
+
+    define: {
+        '__APP_VERSION__': JSON.stringify(process.env.npm_package_version),
+    }
+})
